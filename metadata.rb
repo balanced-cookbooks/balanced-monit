a2ae9c10db1af4feb9171f4d2e64ae5f21989b57
bninja <andrew@poundpay.com>
2014-03-12 17:09:49 -0700

    sem ver

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,6 @@ maintainer_email "matthew@balancedpayments.com"
 license          "All rights reserved"
 description      "Installs/Configures balanced-monit"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "1.0.0"
 
-
-depends 'monit', '~> 1.3.3'
+depends          'monit', '~> 1.3'
